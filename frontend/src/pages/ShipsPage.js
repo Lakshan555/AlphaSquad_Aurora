f24f008e08f556eaa34bb10f9ec2ae5cc8520b08
MadhaviImashi <madhaviimashi@gmail.com>
2023-09-13 23:54:48 +0530

    ship pg updated

--- frontend/src/pages/ShipsPage.js
+++ frontend/src/pages/ShipsPage.js
@@ -27,23 +27,23 @@ export default function ShipsPage() {
     {
       id: 1,
       name: 'Community Ship',
-      shipTypes: ['type1', 'type2', 'type3'],
+      shipTypes: ['Odyssey Shuttle', 'Starlight Commuter', 'CosmoScooter'],
       cost: '$ 3900.00',
-      imageSrc: 'https://s3-alpha-sig.figma.com/img/d258/19cf/fc14e1628c22b14dbab31289862a8dfe?Expires=1695600000&Signature=NlNtANxFk9sVwXDDOUg8LCZRqU7eRJ-QoE4E4Oy-cXTsvhSOe4ORTnLRj4~AgDMq3VDF9fgi57ipq8ly8PmTBrCEZ9bn8G04zF5GijCXTbocDJFVkRCaLNaGuYFB1S7xZGJFCL4bTRLZT99gNLqhIcfk0NyECQC29hj~RdL~tNQnL2cHeja1NOjWDkmogLW14k4jqbMyCJdHmbWTZXjFFwwSNc-bVuGpOOh2NzOnFN5Jd1IoG1zCVdT9g0T2xZjouPFpF~TosCDFO2cybupTwzzsHRJdLKajwiSyBpBdyVH9TOHchopFmL43eXi8yD~xNAU9TWpBpy2GkwnP2p5txw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      imageSrc: 'https://s3-alpha-sig.figma.com/img/0716/8ac3/e5c72b26a1aff62254387cab45180be6?Expires=1695600000&Signature=iZSv~HtyKZnRYeYx4tSxJXU8kSOgqzRfbLY4F4hiYuVYSWyhKX1dtFJlkn~vufD8yvU0JKzxKUAeRErV9xm6YV9nNe9kvCQQD0yTtWnClcxYs2kcWhYmrhI3CIv2-4MprNLk-hZT3c8FX0XlMcGH3nORnUEACCI3HixWWVj4B-ubtFMFJMtvt4FPcEsKPHp0zqudxeIr2ObQrjTSrNJqATmSXeck7IRoqwI2BkdNanoL3bmpnesTwVgQkskoyQRi2z1VJ3OJ6y7FAK7WvcNT1EgX3tN2Sn8~L~HPOGqW~8svfv1QAa3m6c-Y4Kpbjae6f5RAjG1B5AGzT4qM7Wgh5g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
     },
     {
       id: 2,
       name: 'Federation',
       shipTypes: ['type1', 'type2', 'type3'],
       cost: '$ 5000.00',
-      imageSrc: 'https://s3-alpha-sig.figma.com/img/7bad/6a10/0392500f18801957e3a27e7645ba5ed4?Expires=1695600000&Signature=VWpYjE2rt2GD~TLDZZbphOUfgT4Kv2pUHiW9Z~dUjo5nunc6DkWN3z4AK8XpavZIhdBJ785Qy8JJk9omfdRipmWzHaFMSZJPQtXaaMEiuN8RwAv96kTLIV0AxYpdsMTJQvNM4Xw-i2McrQjYZQBQfBFF6bUAL83POgbAsDUdKHDwXWv6xjHtmXKySpDLopJhTmjBknYZfV0vX7Aro6oqLDJiVvFWlCV08P4prRhaASbUBf7sJZrEJYTnLbdl2le5HwJCMHj7WvItgnSVl5e98~CQ4jlUQGutVgfV-wHydFwdNjjY7zU13REXdPI6GaQJ1XBzrVZ~k-gsjno3v3Ao8A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      imageSrc: 'https://s3-alpha-sig.figma.com/img/a857/9090/f21b07661ed5bd7a081fb95f425ff251?Expires=1695600000&Signature=Sd96knPcYCgOZU6-6ZPKwu31gePvgD1Ij736U3mFfq3IqRAxfnWaBm6lEoF2GubT0NhSNUiOiYKH7Z7aOsw5I14W4fIAcNb8MsxM5sIbZLm~C-Dv~7YW1VlA5ZOcDAPF6W0T4qcqbTCO~xvAAoGG7VGv7tMGRMbEa5EMOx8cjHXsjhucKwoqqzK0cNrtYqSZr5hmxoIle6tD0oi3X-ioHjNUqsAqYP-P8uxmIQEzx2zsEbTtWKEkG9MBakTKIaM9kWOWJ6QVneSSTP~C~UzsCjY95JHjZjmSsybYjL359Di3C~4~-Tu9rQIXOR8q8IR3VHMTX4iJ5vQkIAQln~vraQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
     },
     {
         id: 3,
         name: 'Community Ship',
       shipTypes: ['type1', 'type2', 'type3'],
       cost: '$ 3900.00',
-        imageSrc: 'https://s3-alpha-sig.figma.com/img/bbab/5ced/a97dbd661df7b956b37ce251f933c2e7?Expires=1695600000&Signature=ShKlP0LQjCQQuA0Q5oWxFm-otQGSX6zklNCrXzDhZXL6EWxDtAwY1huLcSz2TZ1yj-mo6iGS7IVu4n5K2pjKc8wUI7zIVAmE9PSPW8LgjmmJyJp1qQ3166viMs3-KcjlxuueskxnxtFOeZCAlqfcEmPy3trqQMvjUgWtOfjL-B7jsSLKtw9sKxCW~nzpR6sLu1a7MsTEmk0STGb74svyFqSHKe3EiW3KRe4FDdJF~bltNhZMktMixhHS8n5R~4aMFGSvQqAODCeDjLN9ZfIEuhq-LhuzpfCjFaXqgBU6Z4e5-jrXBi5OwQCUPCR9HnqVIjKZgZ0P7BuJDMPldJrXYQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+        imageSrc: 'https://s3-alpha-sig.figma.com/img/23ef/d0ef/5a1a940ac37d584fa3c08c15a2c86ad4?Expires=1695600000&Signature=YMFWdWYJ4m24T47zHi11aiHrHUmCpm1M3i9XL8rSHcjZTnCJu7IVWquRnb6aoY8YxUMrfL-g~KHt1inGypRdbU4u9EEiNgqMP6qaUz9PCsq-lFg7wd6bb~4SqwAyWVMA2zRManE5F~2QAvJQn-PNVJFKB0S5UUySVxBr0g23OwNNmuT3~D4fthcNbBc31Fy91hzfkxm8xoIyBG02MweDmIVuINjp7Rdmxkj7yvfC-W4q4xsdyIr4tC6VoE4eNEnlRQhTR~FDoIMf34vva9htdpbxGnTvgXkyMb9DxDIZvYnYI0-c7IMfltxTEV9BEEn1LzWNlFcb2E2FntG39FT0IA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
       },
       {
         id: 4,
